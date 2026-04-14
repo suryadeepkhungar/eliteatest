65f409649eb2a38f97191de391432e8fc368d474
suryadeepkhungar <97693869+suryadeepkhungar@users.noreply.github.com>
2026-04-14 18:25:07 +0530

    Create tests/playwright.config.ts

---
+++ tests/playwright.config.ts
@@ -0,0 +1,12 @@
+import { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  timeout: 60000,
+  retries: 0,
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 }
+  }
+};
+
+export default config;
